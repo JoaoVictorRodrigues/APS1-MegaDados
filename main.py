2b239135c2ddf8bddc35cc965d3339e234c6f119
JoaoVictorRodrigues <joaovrs1@al.insper.edu.br>
2020-09-11 12:04:55 -0300

    Crud finalizado

--- main.py
+++ main.py
@@ -11,57 +11,48 @@ class Tarefa(BaseModel):
     desTarefa: str
     finalizado: bool = False
 
+
 dbTarefas = {}
 
 # Mostra todas as notas
 @app.get("/tarefas")
-async def read_root(finalizado: Optional[str] = None):
+async def read_all_tarefas(status: Optional[bool] = None):
+    """ Mostra todas as tarefas. Use o status para filtrar as tarefas finalizadas/não finalizadas """
     if len(dbTarefas) == 0:
         return {"Insira novas tarefas"}
+    elif status != None:
+        dbfiltrado = {}
+        for i in range(len(dbTarefas)):
+            if dbTarefas[i].finalizado == status:
+                dbfiltrado[i] = dbTarefas[i]
+        return dbfiltrado
     else:
         return dbTarefas
 
+
 # Mostra uma unica tarefa
 @app.get("/tarefa/{tarefa_id}")
-async def read_tarefa(tarefa_id: int, q: Optional[str] = None):
+async def read_single_tarefa(tarefa_id: int, q: Optional[str] = None):
+    """ Mostra um unica tarefa """
     selected = dbTarefas[tarefa_id]
     return selected
 
-@app.get("/tarefa/finalisada")
-async def listfinalizado():
-    if len(dbTarefas) == 0:
-        return {"erro 404: nenhuma tarefa"}
-    else:
-        dbFinalizados = {}
-        inc = 0
-        for i in dbTarefas:
-            if dbTarefas[i]["finalisada"]["True"]:
-                dbFinalizados[inc][finalisada]
-                inc+=1
-        return dbFinalizados
-                
-# # Mostra uma unica tarefa
-# @app.get("/tarefa/teste")
-# async def filter_tarefa():
-#     fiil = dbTarefas[0][q = true]
-#     return fiil
-
-
 # Edita uma tarefa
 @app.put("/tarefa/editar/{tarefa_id}")
-async def update_tarefa(tarefa_id: int,tarefa: Tarefa):
-
+async def update_tarefa(tarefa_id: int, tarefa: Tarefa):
+    """ Edita uma tarefa já existente """
     if tarefa_id in dbTarefas:
-        dbTarefas[tarefa_id]= tarefa
+        dbTarefas[tarefa_id] = tarefa
     else:
         return{"Tarefa não encontrada"}
     return dbTarefas
 
-#Cria uma tarefa
+# Cria uma tarefa
 @app.post("/tarefa/nova")
 async def create_tarefa(tarefa: Tarefa):
+    """ Cria uma nova tarefa"""
     if len(dbTarefas) == 0:
-        dbTarefas[0]=tarefa
+        dbTarefas[0] = tarefa
     else:
         dbTarefas[max(dbTarefas.keys())+1] = tarefa
     return len(dbTarefas)
@@ -69,5 +60,6 @@ async def create_tarefa(tarefa: Tarefa):
 # Apaga uma tarefa
 @app.delete("/tarefa/apagar/{tarefa_id}")
 def delete_tarefa(tarefa_id: int):
+    """ Deleta uma tarefa existente"""
     del(dbTarefas[tarefa_id])
     return dbTarefas
