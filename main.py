9c516f7b93a3755bd34ca024bf2a0bba90eea452
JoaoVictorRodrigues <joaovrs1@al.insper.edu.br>
2020-09-22 16:35:19 -0300

    Subindo testes de post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 
@@ -19,7 +19,7 @@ dbTarefas = {}
 async def read_all_tarefas(status: Optional[bool] = None):
     """ Mostra todas as tarefas. Use o status para filtrar as tarefas finalizadas/não finalizadas """
     if len(dbTarefas) == 0:
-        return {"Insira novas tarefas"}
+        return {"msg":"Insira novas tarefas"}
     elif status != None:
         dbfiltrado = {}
         for i in range(len(dbTarefas)):
@@ -41,25 +41,36 @@ async def read_single_tarefa(tarefa_id: int, q: Optional[str] = None):
 @app.put("/tarefa/editar/{tarefa_id}")
 async def update_tarefa(tarefa_id: int, tarefa: Tarefa):
     """ Edita uma tarefa já existente """
-    if tarefa_id in dbTarefas:
-        dbTarefas[tarefa_id] = tarefa
-    else:
-        return{"Tarefa não encontrada"}
-    return dbTarefas
+    try:
+      if tarefa_id in dbTarefas:
+          dbTarefas[tarefa_id] = tarefa
+    except KeyError as exception:
+        raise HTTPException(
+            status_code = 404,
+            detail='Task not found',
+        ) from exception
+
+    
 
 # Cria uma tarefa
 @app.post("/tarefa/nova")
 async def create_tarefa(tarefa: Tarefa):
     """ Cria uma nova tarefa"""
-    if len(dbTarefas) == 0:
-        dbTarefas[0] = tarefa
-    else:
-        dbTarefas[max(dbTarefas.keys())+1] = tarefa
-    return len(dbTarefas)
+    try:
+        if len(dbTarefas) == 0:
+            dbTarefas[0] = tarefa
+        else:
+            dbTarefas[max(dbTarefas.keys())+1] = tarefa
+        return len(dbTarefas)
+    except KeyError as exception:
+        raise HTTPException(
+            status_code = 422,
+            detail='Unprocessable Entity',
+        ) from exception
 
 # Apaga uma tarefa
 @app.delete("/tarefa/apagar/{tarefa_id}")
-def delete_tarefa(tarefa_id: int):
+async def delete_tarefa(tarefa_id: int):
     """ Deleta uma tarefa existente"""
     del(dbTarefas[tarefa_id])
     return dbTarefas
--- test_main.py
+++ test_main.py
@@ -8,3 +8,26 @@ def test_read_main_returns_not_found():
     response = client.get('/')
     assert response.status_code == 404
     assert response.json() == {'detail': 'Not Found'}
+
+
+def test_create_nova_tarefa():
+    response = client.post(
+        "/tarefa/nova",
+        json={"titulo": "Outra Tarefa", "desTarefa": "Descrição de Tarefa"},
+    )
+    assert response.status_code == 200
+
+
+def test_bad_create_nova_tarefa():
+    response = client.post(
+        "/tarefa/nova",
+        json={"desTarefa": "Descrição de Tarefa"},
+    )
+    assert response.status_code == 422
+
+def test_edit_inexistent_tarefa():
+    response = client.put(
+        "/tarefa/editar/10",
+        json={"desTarefa": "Descrição de Tarefa"},
+    )
+    assert response.status_code == 404
