a90163a235a4f1ba2abf38f3b4b8ffc57a1dc91e
JoaoVictorRodrigues <joaovrs1@al.insper.edu.br>
2020-09-22 20:59:40 -0300

    dbsession

--- api/database.py
+++ api/database.py
@@ -1,7 +1,40 @@
+from api.models import Tarefa
+from fastapi import APIRouter, HTTPException, Depends
+
 class DBSession:
-  dbTarefas = {}
-  def __init__(self):
-      self.tasks = DBSession.dbTarefas
+    dbTarefas = {}
+
+    def __init__(self):
+        self.dbTarefas = DBSession.dbTarefas
+
+    def read_all_tarefas(self, status: bool):
+        if len(self.dbTarefas) == 0:
+            return {"msg": "Insira novas tarefas"}
+        elif status != None:
+            dbfiltrado = {}
+            for i in range(len(self.dbTarefas)):
+                if self.dbTarefas[i].finalizado == status:
+                    dbfiltrado[i] = self.dbTarefas[i]
+            return dbfiltrado
+        else:
+            return self.dbTarefas
+
+    def read_single_tarefa(self, tarefa_id: int, q: str):
+        selected = self.dbTarefas[tarefa_id]
+        return selected
+
+    def create_tarefa(self, tarefa: Tarefa):
+        try:
+            if len(self.dbTarefas) == 0:
+                self.dbTarefas[0] = tarefa
+            else:
+                self.dbTarefas[max(self.dbTarefas.keys())+1] = tarefa
+            return len(self.dbTarefas)
+        except KeyError as exception:
+            raise HTTPException(
+                status_code=422,
+                detail='Unprocessable Entity',
+            ) from exception
 
 def get_db():
-  return DBSession()
+    return DBSession()
--- api/routers/task.py
+++ api/routers/task.py
@@ -6,27 +6,18 @@ from api.models import Tarefa
 
 router = APIRouter()
 
+
 @router.get("")
 async def read_all_tarefas(status: Optional[bool] = None, db: DBSession = Depends(get_db)):
     """ Mostra todas as tarefas. Use o status para filtrar as tarefas finalizadas/não finalizadas """
-    if len(db.dbTarefas) == 0:
-        return {"msg": "Insira novas tarefas"}
-    elif status != None:
-        dbfiltrado = {}
-        for i in range(len(db.dbTarefas)):
-            if db.dbTarefas[i].finalizado == status:
-                dbfiltrado[i] = db.dbTarefas[i]
-        return dbfiltrado
-    else:
-        return db.dbTarefas
+    return db.read_all_tarefas(status)
 
 
 # Mostra uma unica tarefa
 @router.get("/{tarefa_id}")
 async def read_single_tarefa(tarefa_id: int, q: Optional[str] = None, db: DBSession = Depends(get_db)):
     """ Mostra um unica tarefa """
-    selected = db.dbTarefas[tarefa_id]
-    return selected
+    return db.read_single_tarefa(tarefa_id)
 
 # Edita uma tarefa
 
@@ -48,17 +39,7 @@ async def update_tarefa(tarefa_id: int, tarefa: Tarefa, db: DBSession = Depends(
 @router.post("/nova")
 async def create_tarefa(tarefa: Tarefa, db: DBSession = Depends(get_db)):
     """ Cria uma nova tarefa"""
-    try:
-        if len(db.dbTarefas) == 0:
-            db.dbTarefas[0] = tarefa
-        else:
-            db.dbTarefas[max(db.dbTarefas.keys())+1] = tarefa
-        return len(db.dbTarefas)
-    except KeyError as exception:
-        raise HTTPException(
-            status_code=422,
-            detail='Unprocessable Entity',
-        ) from exception
+    return db.create_tarefa(tarefa)
 
 # Apaga uma tarefa
 
